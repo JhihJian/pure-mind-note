5e16b40e9436e629a809b06854dbec4cd0b30b42
zhengzhijian <zhengzhijian@anciert.com>
2025-04-25 17:36:33 +0800

    调整TypeScript配置，禁用未使用变量和参数的检查；更新Vite配置，移除测试相关设置，简化插件配置。

--- pure-mind-note/vite.config.ts
+++ pure-mind-note/vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { configDefaults, defineConfig as defineVitestConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-  }
+  plugins: [react()]
 })
